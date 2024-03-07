75438a57598119e0a66f03244a8af0e429e7f2fa
ramonsilva186 <ramonsilva186@gmail.com>
2024-03-07 20:01:21 -0300

    project created

---
+++ src/test/java/com/simplify/ChallengeTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
