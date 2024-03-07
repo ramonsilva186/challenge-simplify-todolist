2f22bdc42ddf23e640e67b1158f3076f179a6994
ramonsilva186 <ramonsilva186@gmail.com>
2024-03-07 20:39:08 -0300

    create todo service crud

---
+++ src/main/java/com/simplify/service/TodoService.java
@@ -0,0 +1,37 @@
+package com.simplify.service;
+
+import com.simplify.entity.Todo;
+import com.simplify.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    //Tambem pode ser feito a injeção de dependencia pelo construtor
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+}
