abb127304c4d0e820f1019a8a1f0d8aeca53bea4
ramonsilva186 <ramonsilva186@gmail.com>
2024-03-07 20:57:30 -0300

    create todo controller

---
+++ src/main/java/com/simplify/controller/TodoController.java
@@ -0,0 +1,33 @@
+package com.simplify.controller;
+
+import com.simplify.entity.Todo;
+import com.simplify.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+    @PostMapping()
+    public List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+    @GetMapping()
+    public List<Todo> list() {
+        return todoService.list();
+    }
+    @PutMapping()
+    public List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+    @DeleteMapping("{id}")
+    public List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+
+}
