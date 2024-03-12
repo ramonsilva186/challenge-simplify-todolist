714e7c9a4644dc93aec55ec0074006b9806da0c9
ramonsilva186 <ramonsilva186@gmail.com>
2024-03-12 15:56:47 -0300

    Add test todo success

--- src/main/java/com/simplify/entity/Todo.java
+++ src/main/java/com/simplify/entity/Todo.java
@@ -1,13 +1,17 @@
 package com.simplify.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "todos")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +20,11 @@ public class Todo {
     private String descricao;
     private boolean realizado;
     private int prioridade;
+
+    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
 }
